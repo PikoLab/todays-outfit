de7c189566925838986a17caf8238695e7204c7e
KU NUNG Chen <kunungchen@KUs-MacBook-Air.local>
2021-12-03 21:20:18 +0800

    add website code

---
+++ Web/server/static/wish.js
@@ -0,0 +1,35 @@
+const container = document.getElementById('container')
+container.addEventListener('click', (event) => {
+  if (event.target.id === "btn-add") {
+    event.target.id = "btn-non"
+    event.target.class = "wish_item_non"
+    event.target.style = "color:#B6AD90;"
+    let outfitId = event.target.value
+    fetch('/removewish', {
+      method: 'post',
+      headers: {
+        'content-type': 'application/x-www-form-urlencoded'
+      },
+      body: JSON.stringify(
+        {'outfit_id': outfitId}
+      )
+    })
+  } else if (event.target.id === "btn-non") {
+    event.target.id = "btn-add"
+    event.target.class = "wish_item_add"
+    event.target.style = "color:#E76F51;"
+    let outfitId = event.target.value 
+    console.log(outfitId)
+    fetch('/addwish', {
+      method: 'post',
+      headers: {
+        'content-type': 'application/x-www-form-urlencoded'
+      },
+      body: JSON.stringify(
+        {'outfit_id': outfitId}
+      )
+    })
+  } else {
+    // pass
+  }
+})  
