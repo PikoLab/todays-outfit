b936c5f21788490208d2b7ef33fa200e01db37a7
PikoLab <nonie.115@gmail.com>
2021-12-20 16:09:03 +0800

    wordcloud: add stop words and corpus

--- 01_Data_Pipeline/dags/datapipeline_dag_production.py
+++ 01_Data_Pipeline/dags/datapipeline_dag_production.py
@@ -12,8 +12,8 @@ default_args = {
     'depends_on_past': False,
     'start_date': AIRFLOW_START_DATE,
     'email': [AIRFLOW_EMIAL],
-    'email_on_failure': False,
-    'email_on_retry': False,
+    'email_on_failure': True,
+    'email_on_retry': True,
     'retries': 1,
     'retry_delay': timedelta(minutes=5),
 }
